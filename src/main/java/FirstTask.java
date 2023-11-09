46cbf4ce9d1bfecddbff6f3e3adb229fc45a1818
Tapka3000 <circle.table@yandex.ru>
2023-11-09 20:13:00 +0300

    Commit

--- src/main/java/FirstTask.java
+++ src/main/java/FirstTask.java
@@ -1,10 +1,11 @@
 public class FirstTask {
     public static void main(String[] args) {
-        convertNumber(15);
+        int num = 15;
+        System.out.println(convertNumber(num));
     }
+
     public static String convertNumber(int number) {
-        String result = (number > 0) ? (number % 7 < 4 ? "positive-m" : "positive-p") : (number == 0 ? "zero" : number % 7 > -4 ? "negative-m" : "negative-p");
-        System.out.println(result);
-        return result;
+        return number > 0 ? (number % 7 < 4 ? "positive-m" : "positive-p")
+                : number == 0 ? "zero" : (number % 7 > -4 ? "negative-m" : "negative-p");
     }
 }
